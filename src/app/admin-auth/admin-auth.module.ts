509d57920b78f6d01f10a4aade98aa74b65b0fe4
Golam Fazle Rabbi <napster@Golams-MacBook-Air.local>
2023-08-19 01:34:38 +0700

    folder structure

---
+++ src/app/admin-auth/admin-auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminAuthRuteModule } from './admin-auth-rute.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminAuthRuteModule
+  ]
+})
+export class AdminAuthModule { }
